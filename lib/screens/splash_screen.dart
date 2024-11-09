836ade79594c1881a060f2c555afb98ac3accd95
DemonSheriff <anantshukla404@gmail.com>
2024-11-09 15:56:53 +0530

    added smooth splash screen transitions

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -6,63 +6,113 @@ class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() {
-    return _SplashScreenState();
-  }
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+
   @override
   void initState() {
     super.initState();
 
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+
+    // _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
+    _controller.forward();
+
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.immersive,
+      overlays: SystemUiOverlay.values,
+    );
+
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
-          builder: (context) {
-            return const ImageGridScreen();
-          },
+          builder: (context) => const ImageGridScreen(),
         ),
       );
     });
   }
 
   @override
-  Widget build(BuildContext context) {
+  void dispose() {
+    _controller.dispose();
     SystemChrome.setEnabledSystemUIMode(
       SystemUiMode.manual,
       overlays: SystemUiOverlay.values,
     );
+    super.dispose();
+  }
 
-    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.black87,
       body: Container(
         width: double.infinity,
         decoration: const BoxDecoration(
-          // gradient: LinearGradient(
-          //   begin: Alignment.topLeft,
-          //   end: Alignment.bottomRight,
-          //   colors: [
-          //     Color.fromARGB(255, 47, 54, 64),
-          //     Colors.black,
-          //   ],
-          // ),
-          color: Colors.black,
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.0, 0.3, 0.7, 1.0],
+            colors: [
+              Color(0xFF0F0F0F), 
+              Color(0xFF090909), 
+              Color(0xFF050505), 
+              Color(
+                  0xFF020202), 
+            ],
+          ),
         ),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(
-              image: AssetImage('assets/github-mark/github-mark-white.png'),
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            
-            CircularProgressIndicator(
-              color: Colors.white,
-            ),
-          ],
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: const Text(
+                  'Github Repo Search',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 36,
+                    color: Colors.white,
+                    letterSpacing: 1.2,
+                    shadows: [
+                      Shadow(
+                        color: Colors.white24,
+                        offset: Offset(0, 2),
+                        blurRadius: 4,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              const Image(
+                image: AssetImage('assets/github-mark/github-mark-white.png'),
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(height: 80),
+              const CircularProgressIndicator(
+                color: Colors.white,
+                strokeWidth: 3,
+              ),
+            ],
+          ),
         ),
       ),
     );
